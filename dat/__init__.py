41cbb4f2e0c01757757922471f74791248ad9588
christopherpryer <christophpryer@gmail.com>
2019-09-09 10:45:22 -0400

    added historiclas_json_to_df to search

--- dat/__init__.py
+++ dat/__init__.py
@@ -15,7 +15,7 @@ class Session(Dat):
         self.connect()
 
     def connect(self):
-        """create session with Dat Rateview via http"""
+        """Create session with Dat Rateview via http."""
         self.session.post(self.login_url, data=self.token)
 
 class Helper(Session):
@@ -35,9 +35,40 @@ class Helper(Session):
            }
         return self.session.post(self.historicals_url, json=payload).json()
 
+    @staticmethod
+    def historicals_json_to_df(json):
+        columns = ['origin_geography', 'destination_geography', 'miles',
+            'origin_id', 'origin_text', 'destination_id', 'destination_text',
+            'equipment_category', 'rate_type', 'average_linehaul',
+            'high_linehaul', 'low_linehaul', 'average_fuel_surcharge',
+            'contributors', 'contributions', 'days_back']
+        data = []
+        for idx in range(0, len(json['result'])):
+            values = json['result'][idx]
+            data.append(
+                [values['originGeography'],
+                values['destinationGeography'],
+                values['miles'],
+                values['actualLane']['origin']['id'],
+                values['actualLane']['origin']['text'],
+                values['actualLane']['destination']['id'],
+                values['actualLane']['destination']['text'],
+                values['actualLane']['equipmentCategory'],
+                values['marketRate']['type'],
+                values['marketRate']['averageLinehaulRate']['value'],
+                values['marketRate']['highLinehaulRate']['value'],
+                values['marketRate']['lowLinehaulRate']['value'],
+                values['marketRate']['averageFuelSurcharge']['value'],
+                values['marketRate']['contributors'],
+                values['marketRate']['contributions'],
+                values['daysBack']]
+            )
+        return pd.DataFrame(data=data, columns=columns)
+
     def search(self, origin_str, dest_str):
-        """using search_str ('city, state, zip') get json of historical data
-        from Dat"""
+        """Using search_str ('city, state, zip') get dataframe of historical
+        data from Dat."""
         origin_id = self.search_locations(origin_str)[0]['handle']
         dest_id = self.search_locations(dest_str)[0]['handle']
-        return self.search_historicals(origin_id, dest_id)
+        historical_data = self.search_historicals(origin_id, dest_id)
+        return self.historicals_json_to_df(historical_data)
--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ from dat import Helper
 import os
 
 
-class TestDatCase(unittest.TestCase):
+class DatHelperCase(unittest.TestCase):
     def setUp(self):
         self.username = os.environ['DAT_USERNAME']
         self.password = os.environ['DAT_PASSWORD']
@@ -26,18 +26,26 @@ class TestDatCase(unittest.TestCase):
     def test_search_historicals(self):
         helper = Helper(self.token)
         origin = 'Cherry Hill, NJ, 08002'
-        dest = 'Cherry Hill, NJ, 08002'
+        dest = 'Philadelphia, PA, 19106'
         origin_id = helper.search_locations(origin)[0]['handle']
         dest_id = helper.search_locations(dest)[0]['handle']
         historical_data = helper.search_historicals(origin_id, dest_id)
         self.assertTrue('result' in historical_data)
 
+    def test_historicals_json_to_df(self):
+        helper = Helper(self.token)
+        origin = 'Cherry Hill, NJ, 08002'
+        dest = 'Philadelphia, PA, 19106'
+        historical_json = helper.search(origin, dest)
+        df = helper.historicals_json_to_df(historical_json)
+        self.assertTrue(not df.empty)
+
     def test_search(self):
         helper = Helper(self.token)
         origin = 'Cherry Hill, NJ, 08002'
-        dest = 'Cherry Hill, NJ, 08002'
-        historical_data = helper.search(origin, dest)
-        self.assertTrue('result' in historical_data)
+        dest = 'Philadelphia, PA, 19106'
+        df = helper.search(origin, dest)
+        self.assertTrue(not df.empty)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
