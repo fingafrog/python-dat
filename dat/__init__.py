f90fc03344d5c3bc32a8f019dbcc826a61681d62
christopherpryer <christophpryer@gmail.com>
2019-09-09 10:28:49 -0400

    added search

--- dat/__init__.py
+++ dat/__init__.py
@@ -5,6 +5,8 @@ class Dat:
     login_url = 'https://rateview.dat.com/api/session/login'
     locations_url = 'https://rateview.dat.com/api/location/suggestions'
     historicals_url = 'https://rateview.dat.com/api/search/getRateSearch'
+    rate_market = 'Spot'
+    rate_type = 'PerTrip'
 
 class Session(Dat):
     def __init__(self, token):
@@ -17,8 +19,25 @@ class Session(Dat):
         self.session.post(self.login_url, data=self.token)
 
 class Helper(Session):
-    def get_location(self, search_str):
+    def search_locations(self, search_str):
         """Return json for search_str ('city, state, zip') to get location id
         using Dat to find best match."""
-        data = {'term': search_str}
-        return self.session.post(self.locations_url, json=data).json()
+        payload = {'term': search_str}
+        return self.session.post(self.locations_url, json=payload).json()
+
+    def search_historicals(self, origin_id, dest_id, equipment='Dry'):
+        payload = {
+            'rateMarket': self.rate_market,
+            'rateType': self.rate_type,
+            'equipmentCategory': equipment,
+            'originHandle': origin_id,
+            'destinationHandle': dest_id
+           }
+        return self.session.post(self.historicals_url, json=payload).json()
+
+    def search(self, origin_str, dest_str):
+        """using search_str ('city, state, zip') get json of historical data
+        from Dat"""
+        origin_id = self.search_locations(origin_str)[0]['handle']
+        dest_id = self.search_locations(dest_str)[0]['handle']
+        return self.search_historicals(origin_id, dest_id)
--- tests.py
+++ tests.py
@@ -8,21 +8,36 @@ class TestDatCase(unittest.TestCase):
     def setUp(self):
         self.username = os.environ['DAT_USERNAME']
         self.password = os.environ['DAT_PASSWORD']
+        self.token = {'userName': self.username, 'password': self.password}
 
     def tearDown(self):
         pass
 
     def test_connect(self):
-        token = {'userName': self.username, 'password': self.password}
-        helper = Helper(token)
+        helper = Helper(self.token)
         self.assertTrue('.TCAUTH' in helper.session.cookies.get_dict())
 
     def test_search_locations(self):
-        token = {'userName': self.username, 'password': self.password}
-        session = Helper(token)
+        helper = Helper(self.token)
         location = 'Cherry Hill, NJ, 08002'
-        location_data = session.get_location(location)
+        location_data = helper.search_locations(location)
         self.assertTrue('handle' in location_data[0])
+
+    def test_search_historicals(self):
+        helper = Helper(self.token)
+        origin = 'Cherry Hill, NJ, 08002'
+        dest = 'Cherry Hill, NJ, 08002'
+        origin_id = helper.search_locations(origin)[0]['handle']
+        dest_id = helper.search_locations(dest)[0]['handle']
+        historical_data = helper.search_historicals(origin_id, dest_id)
+        self.assertTrue('result' in historical_data)
+
+    def test_search(self):
+        helper = Helper(self.token)
+        origin = 'Cherry Hill, NJ, 08002'
+        dest = 'Cherry Hill, NJ, 08002'
+        historical_data = helper.search(origin, dest)
+        self.assertTrue('result' in historical_data)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
